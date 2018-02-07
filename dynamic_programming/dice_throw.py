27834a4eacc4063c05cce3e53a28d6e38a0ffcae
adityachatterjee <adityachatterjee42@gmail.com>
2018-02-06 22:56:13 -0800

    dice throw

---
+++ dynamic_programming/dice_throw.py
@@ -0,0 +1,19 @@
+memo = {}
+
+def dice_throw(sumleft, diceleft):
+    state = "{0}-{1}".format(sumleft, diceleft)
+    if state in memo:
+        return memo[state]
+    if sumleft == 0 and diceleft == 0:
+        return 1
+    if sumleft < 0 or diceleft == 0:
+        return 0
+    ways = 0
+    for i in range(1,7):
+        ways = ways + dice_throw(sumleft-i, diceleft-1)
+    memo[state] = ways
+    return ways
+
+print(dice_throw(6,2))
+
+
